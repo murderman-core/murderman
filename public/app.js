d24f8153b9fae88f6ddf2a0e0b12cf17a18fc8ff
Oliver Zhang <ozhanghe@gmail.com>
2020-09-19 17:53:04 -0700

    Added canvas

--- public/app.js
+++ public/app.js
@@ -1,6 +1,18 @@
 var socket;
+var canvas;
+var gl;
 
 function init()
 {
     socket = io.connect("https://murderman.herokuapp.com");
+    canvas = document.getElementById("game");
+    gl = canvas.getContext("webgl");
+
+    if (gl === null) {
+        alert("Sorry! It looks like our game failed to initialize. This may be caused by you machine not supporting webgl.");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
