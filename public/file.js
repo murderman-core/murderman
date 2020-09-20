67b7e084ba040f422cb2f47c0d58f34afdde055a
Oliver Zhang <ozhanghe@gmail.com>
2020-09-19 20:04:58 -0700

    Fixed File I/O

--- public/file.js
+++ public/file.js
@@ -1,8 +1,18 @@
 function loadFromFile(path) 
 {
-    var fr = new FileReader();
-
-    fr.readAsText(path);
-
-    return fr.result;
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                return allText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return null;
 }
